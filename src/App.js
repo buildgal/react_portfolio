013adc6d47f27787ee082da864fe41f33c41a709
buildgal <gjaquelin7@outlook.com>
2021-03-26 14:53:44 -0500

    switch pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Projects from "./components/Jumbo/index.js";
 import Header from "./components/Header/index.js";
 import Footer from "./components/Footer/index.js";
-import Contact from "./pages/Contact";
-import AboutMe from "./pages/about";
-import Projects from "./pages/projects";
-import Home from "./pages/home";
+import Contact from "./pages/Contact.js";
+import AboutMe from "./pages/about.js";
+import Projects from "./pages/projects.js";
+import Home from "./pages/home.js";
 
 
 
@@ -16,12 +16,14 @@ function App() {
   return (
     <div className="App">
       <Router>
+      <Switch>
       <Header>
       <Route exact path={["/","/Home"]} component={Home} />
         <Route exact path="/Contact" component={Contact} />
         <Route exact path="/AboutMe" component={AboutMe} />
         <Route exact path="/Projects" component={Projects} />
       </Header>
+      </Switch>
       </Router>
 
 
